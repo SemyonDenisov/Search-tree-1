ec7c9e8ce91c6b4e3d2f5cee6bfa58ea09cb17d9
SemyonDenisov <semdenis2003@mail.ru>
2022-03-03 17:13:34 +0400

    correct print method

--- Source.cpp
+++ Source.cpp
@@ -17,7 +17,7 @@ public:
 	{
 		root = NULL;
 	}
-	void print()
+	/*void print()
 	{
 		node* temp_root = root;
 		while (temp_root)
@@ -43,6 +43,17 @@ public:
 				else
 					break;
 			}
+		}
+	}*/
+	void print()
+	{
+		treeprint(root);
+	}
+	void treeprint(node* root) {
+		if (root != NULL) { //Пока не встретится пустой узел
+			cout << "Key - " << root->key << " String - " << root->value << endl; //Отображаем корень дерева
+			treeprint(root->left); //Рекурсивная функция для левого поддерева
+			treeprint(root->right); //Рекурсивная функция для правого поддерева
 		}
 	}
 	bool insert(int key, string value)
@@ -73,12 +84,11 @@ public:
 			root->key = key;
 			root->value = value;
 		}
-		else if (key < root->key)   // условие добавление левого потомка
+		else if (key < root->key)
 			root->left = call_insert(key, value,root->left);
-		else    // условие добавление правого потомка
+		else 
 			root->right = call_insert(key, value, root->right);
 		return(root);
-
 	}
 	node* find(int key)
 	{
@@ -111,7 +121,9 @@ int main()
 	A.insert(2, "Pops");
 	A.insert(-1, "hueps");
 	A.insert(-2, "d");
-	A.insert(3, "Styopakazel");
+	A.insert(6, "Styopakazel");
+	A.insert(3, "check");
+	A.insert(0, "check_1");
 	A.print();
 	return 0;
 }
