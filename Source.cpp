dc0c5c08f185240db8f2308a003c9164a446cd35
SemyonDenisov <91726358+SemyonDenisov@users.noreply.github.com>
2022-03-15 16:29:53 +0400

    correct method erase

--- Source.cpp
+++ Source.cpp
@@ -2,18 +2,6 @@
 int main()
 {
 	Map A;
-	A.insert(1,"Senja");
-	A.insert(2, "Pops");
-	A.insert(-1, "hueps");
-	A.insert(-2, "d");
-	A.insert(6, "Styopakazel");
-	A.insert(3, "check");
-	A.insert(0, "check_1");
-	string res = A.find(1);
-	A.erase(1);
-	A.erase(2);
-	cout << res<< endl;
-	A.print();
 	bool ex = true;
 	int n;
 	while (ex)
